d7b4c90e22bbd4525be18eac26c51293144b51d0
Dominic <dominic@hainstech.com>
2022-03-31 23:31:36 -0400

    feat: desktop notifications

--- src/duration.rs
+++ src/duration.rs
@@ -1,7 +1,9 @@
+use notify_rust::Notification;
 use std::fmt;
+
 pub struct Duration {
     name: String,
-    duration: u64,
+    work_duration: u64,
     current_duration: u64,
     break_duration: u64,
     working: bool,
@@ -16,13 +18,13 @@ impl fmt::Display for Duration {
 }
 
 impl Duration {
-    pub fn new(name: String, duration_mins: u64, break_duration_mins: u64) -> Self {
-        let duration = duration_mins * 60;
+    pub fn new(name: String, work_duration_mins: u64, break_duration_mins: u64) -> Self {
+        let work_duration = work_duration_mins * 60;
         let break_duration = break_duration_mins * 60;
         Self {
             name,
-            duration,
-            current_duration: duration,
+            work_duration,
+            current_duration: work_duration,
             break_duration,
             working: true,
         }
@@ -38,11 +40,19 @@ impl Duration {
 
     pub fn change_period(&mut self) {
         self.working = !self.working;
+        let notification_msg: String;
         if self.working {
-            self.current_duration = self.duration;
+            self.current_duration = self.work_duration;
+            notification_msg = String::from("Time to work!");
         } else {
             self.current_duration = self.break_duration;
+            notification_msg = String::from("Break time!");
         }
+        Notification::new()
+            .summary("Pom")
+            .body(&notification_msg)
+            .show()
+            .unwrap();
     }
 
     pub fn get_status(&self) {
